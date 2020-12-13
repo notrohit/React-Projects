ca45f5b434b1c55dff1a41f5de2c4d28f35e3e26
notrohit <rohitsingh250193@gmail.com>
2020-12-13 13:38:44 +0530

    add toggle question functionality based on state and conditional rendering

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ( {title, info} ) => {
+  const [showInfo, setShowInfo] = useState(false)
+  // can be done by a simple inline function at btn event listener
+  const toggleQuestion = (showInfo)=>{
+    setShowInfo(!showInfo)
+  }
+  return <article className="question">
+    <header>
+      <h4 >{title}</h4>
+      {/* {console.log(title )} */}
+      <button onClick={()=>toggleQuestion(showInfo)} className="btn">
+      {/* Alternate method to call inline function and setState inline
+       <button onClick={()=>setShowInfo(! showInfo)} className="btn"> */}
+        {showInfo? <AiOutlineMinus /> : <AiOutlinePlus /> }
+      </button>
+    </header>
+      { showInfo && <p>{info}</p>}
+
+  </article>;
 };
 
 export default Question;
