fe3dea2b19a43448acc1584a833bab7c47c5569b
notrohit <rohitsingh250193@gmail.com>
2020-12-13 15:11:02 +0530

    add dynamic filtering based on product  categories

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,7 +4,31 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  
+  const allCategories = ['all', ...new Set(items.map(item=>item.category))]
+  console.log(allCategories);
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) =>{
+    if(category ==='all'){
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item)=>item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h3>Our Menu</h3>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </section>
+  </main>;
 }
 
 export default App;
